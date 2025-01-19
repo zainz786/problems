3a29399401c69ef92d35f3d513d8886ed1b01975
Zain <zain_zahid786@hotmail.co.uk>
2025-01-19 16:04:04 +0000

    updated passwords.ts with notes

--- passwords.ts
+++ passwords.ts
@@ -1,15 +1,20 @@
 function passValidate(pass:string): boolean{
     let valid: boolean = true
+    //creates an expression to show there is atleast 1 uppercase 
     const upperCase = new RegExp(/(?=.*[A-Z])/g)
+    //creates an expression to show there is atleast 1 lowercase
     const lowerCase = new RegExp(/(?=.*[a-z])/g)
+    //creates an expression to show there is atleast 1 number
     const numeric = new RegExp(/(?=.*[0-9])/g)
     //Simple check before validating contents of password
     if (pass.length < 8){
         valid = false;
     }
+    //validating the contents of the password
     else if (pass.match(upperCase) && pass.match(lowerCase) && pass.match(numeric)) {
         valid= true;
     }
+    //to catch cases that are out of scop of the question
     else{
         valid= false;
     }
